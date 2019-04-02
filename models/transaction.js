cbd9ecccfcaa1e5dc804df80d5f35ed36b990dcd
vyonizr <fitrahtur@gmail.com>
2019-04-02 23:51:16 +0700

    Release 5

--- models/transaction.js
+++ models/transaction.js
@@ -16,6 +16,20 @@ const transactionSchema = new Schema({
   }]
 })
 
+transactionSchema.post("validate", function(transaction) {
+  const oneDay = 24*60*60*1000;
+  const in_date = new Date(transaction.in_date);
+  const due_date = new Date(transaction.due_date);
+
+  const diffDays = Math.round(((in_date.getTime() - due_date.getTime())/(oneDay)));
+  if (diffDays > 0) {
+    transaction.fine = diffDays * 1000
+  }
+  else {
+    transaction.fine = 0
+  }
+})
+
 transactionSchema.post("findOneAndUpdate", function(transaction) {
   const oneDay = 24*60*60*1000;
   const in_date = new Date(transaction.in_date);
